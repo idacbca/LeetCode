f7d738fd2a5fa1230b58b1162cf5c06c283ef5d8
acbca <idacbca@126.com>
2021-03-03 18:11:10 +0800

    add no.92

---
+++ Solution/src/Linked_list/No92ReverseLinkedList.java
@@ -0,0 +1,36 @@
+package Linked_list;
+
+public class No92ReverseLinkedList {
+
+    /* Definition for singly-linked list. */
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    ListNode successor = null;
+
+    private ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next, n - 1);
+
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        ListNode last = reverseBetween(head.next, left - 1, right - 1);
+        head.next = last;
+        return head;
+    }
+}
