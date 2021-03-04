ec2b0344b984f3b9946381f8ac3c555c661cad42
acbca <idacbca@126.com>
2021-03-04 18:06:14 +0800

    add No116 iterate

--- Solution/src/No116.java
+++ Solution/src/No116.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class No116 {
     private class Node {
         public int val;
@@ -34,5 +37,28 @@ public class No116 {
         connectNode(n1.left, n1.right);
         connectNode(n2.left, n2.right);
         connectNode(n1.right, n2.left);
+    }
+
+    /* Iterate connect */
+    public Node iterateConnect(Node root) {
+        if (root == null) return null;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i ++) {
+                Node node = queue.poll();
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return root;
     }
 }
