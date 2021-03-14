b7e0e27e9af018860efed8653aaa42caae9b6a3f
acbca <idacbca@126.com>
2021-03-14 12:04:15 +0800

    update No1143

--- Solution/src/No1143.java
+++ Solution/src/No1143.java
@@ -30,8 +30,26 @@ public class No1143 {
         return memo[i][j];
     }
 
-    /* State compression, space O(n) */
+    /* Iterate */
     public int longestCommonSubsequence2(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] memo = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i ++) {
+            for (int j = 1; j <= n; j ++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    memo[i][j] = 1 + memo[i - 1][j - 1];
+                } else {
+                    memo[i][j] = Math.max(memo[i][j - 1], memo[i - 1][j]);
+                }
+            }
+        }
+        return memo[m][n];
+    }
+
+    /* State compression, space O(n) */
+    public int longestCommonSubsequence3(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
         int[] memo = new int[n + 1];
