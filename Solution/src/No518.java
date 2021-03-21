83912df5440cfd308d5f534947b69824a5bfe4f9
acbca <idacbca@126.com>
2021-03-21 11:48:18 +0800

    update No518

--- Solution/src/No518.java
+++ Solution/src/No518.java
@@ -17,4 +17,20 @@ public class No518 {
         }
         return dp[n][amount];
     }
+
+    /* State Compression */
+    public int change1(int amount, int[] coins) {
+        int n = coins.length;
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i ++) {
+            for (int j = 1; j <= amount; j ++) {
+                if (j - coins[i - 1] >= 0) {
+                    dp[j] = dp[j] + dp[j - coins[i - 1]];
+                }
+            }
+        }
+        return dp[amount];
+    }
 }
