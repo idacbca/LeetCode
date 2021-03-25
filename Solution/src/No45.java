6af9d96690c3ef6b64f8fd9e8db0c3e84fe4a607
acbca <idacbca@126.com>
2021-03-25 14:08:55 +0800

    add No45

---
+++ Solution/src/No45.java
@@ -0,0 +1,15 @@
+public class No45 {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int end = 0, farthest = 0;
+        int jumps = 0;
+        for (int i = 0; i < n - 1; i ++) {
+            farthest = Math.max(nums[i] + i, farthest);
+            if (end == i) {
+                jumps ++;
+                end = farthest;
+            }
+        }
+        return jumps;
+    }
+}
