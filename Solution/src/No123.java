10dfe1f038472ea3b47da90881e50d05d5379240
acbca <idacbca@126.com>
2021-03-26 13:42:09 +0800

    update No123

--- Solution/src/No123.java
+++ Solution/src/No123.java
@@ -1,5 +1,24 @@
 public class No123 {
+    /* dp */
     public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int maxK = 2;
+        int[][][] dp = new int[n][maxK + 1][2];
+
+        for (int i = 0; i < n; i ++) {
+            for (int k = maxK; k >= 1; k --) {
+                if (i - 1 == -1) {
+                    dp[i][k][1] = -prices[i];
+                } else {
+                    dp[i][k][0] = Math.max(dp[i-1][k][0], dp[i-1][k][1] + prices[i]);
+                    dp[i][k][1] = Math.max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i]);
+                }
+            }
+        }
+        return dp[n-1][maxK][0];
+    }
+    /* dp optimized */
+    public int maxProfit1(int[] prices) {
         int dp_i10 = 0, dp_i11 = Integer.MIN_VALUE;
         int dp_i20 = 0, dp_i21 = Integer.MIN_VALUE;
 
