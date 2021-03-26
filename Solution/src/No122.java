660b019043d95d7fa4e729541734dbb2f683ab36
acbca <idacbca@126.com>
2021-03-26 12:22:04 +0800

    add No122

---
+++ Solution/src/No122.java
@@ -0,0 +1,12 @@
+public class No122 {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int price : prices) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + price);
+            dp_i_1 = Math.max(dp_i_1, dp_i_0 - price);
+        }
+        return dp_i_0;
+    }
+}
