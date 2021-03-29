49de0baa659f61787066ec63aa2971c3e09cd13e
acbca <idacbca@126.com>
2021-03-29 10:27:49 +0800

    add No337

---
+++ Solution/src/No337.java
@@ -0,0 +1,30 @@
+public class No337 {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int rob(TreeNode root) {
+        int[] res = dp(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] dp(TreeNode root) {
+        if (root == null) {
+            return new int[]{0, 0};
+        }
+        int[] left = dp(root.left);
+        int[] right = dp(root.right);
+        int rob = left[1] + right[1] + root.val;
+        int not_rob = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return new int[]{rob, not_rob};
+    }
+}
