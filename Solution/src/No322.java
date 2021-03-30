3ae8abfc8fdfc34cb47b8cb6f9dc3076f0af45a5
acbca <idacbca@126.com>
2021-03-30 11:25:59 +0800

    add No322 traditional dp solution

--- Solution/src/No322.java
+++ Solution/src/No322.java
@@ -14,4 +14,27 @@ public class No322 {
         }
         return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
+
+    /* Slow */
+    public int coinChange1(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int[] i : dp) {
+            Arrays.fill(i, amount + 1);
+        }
+        for (int i = 0; i <= n; i ++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 1; i <= n; i ++) {
+            for (int j = 1; j <= amount; j ++) {
+                if (j >= coins[i - 1]) {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i - 1]] + 1);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][amount] == amount + 1 ? -1 : dp[n][amount];
+    }
 }
