e5143df424f3c4ba44ab1a90dce0a3437421e8e1
acbca <idacbca@126.com>
2021-04-09 23:17:46 +0800

    update No81

--- Solution/src/No81.java
+++ Solution/src/No81.java
@@ -5,6 +5,36 @@ public class No81 {
         if (n == 1) {
             return nums[0] == target;
         }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] > nums[r]) {
+                if (nums[l] <= target && nums[mid] > target) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else if (nums[mid] < nums[r]) {
+                if (nums[mid] < target && nums[r] >= target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            } else {
+                r --;
+            }
+        }
+        return false;
+    }
+
+    public boolean search2(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) return false;
+        if (n == 1) {
+            return nums[0] == target;
+        }
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
