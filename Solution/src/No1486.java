7336c0357669d0225c88bfbc3f86aba0a8ac04e4
acbca <idacbca@126.com>
2021-05-07 13:04:23 +0800

    add No1486

---
+++ Solution/src/No1486.java
@@ -0,0 +1,9 @@
+public class No1486 {
+    public int xorOperation(int n, int start) {
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans ^= (start + 2 * i);
+        }
+        return ans;
+    }
+}
