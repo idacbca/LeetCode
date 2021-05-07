27da9244bc7f8ee6c6aea6b0b18795da432624d3
acbca <idacbca@126.com>
2021-05-07 14:44:40 +0800

    add No130

---
+++ Solution/src/No130.java
@@ -0,0 +1,37 @@
+public class No130 {
+    /* DFS O(m * n) */
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int x, int y) {
+        int m = board.length;
+        int n = board[0].length;
+        if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] != 'O') {
+            return;
+        }
+        board[x][y] = 'A';
+        dfs(board, x - 1, y);
+        dfs(board, x + 1, y);
+        dfs(board, x, y - 1);
+        dfs(board, x, y + 1);
+    }
+}
