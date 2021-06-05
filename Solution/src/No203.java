cdf8991fb2830cffe9dd698199a27976802afac2
acbca <idacbca@126.com>
2021-06-05 18:26:53 +0800

    add No203

---
+++ Solution/src/No203.java
@@ -0,0 +1,11 @@
+public class No203 {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        if (head.val == val) {
+            head = removeElements(head.next, val);
+        } else {
+            head.next = removeElements(head.next, val);
+        }
+        return head;
+    }
+}
