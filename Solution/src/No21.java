ce46c4ff246f3e8806459e3bb526a1ac8972859a
acbca <idacbca@126.com>
2021-08-01 16:51:11 +0800

    update No21

--- Solution/src/No21.java
+++ Solution/src/No21.java
@@ -15,4 +15,18 @@ public class No21 {
         prev.next = l1 == null ? l2 : l1;
         return preHead.next;
     }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
 }
