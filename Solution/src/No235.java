624b54153d64c169b94502c542fb4d66a39d4ac5
acbca <idacbca@126.com>
2021-08-03 22:04:03 +0800

    update No235

---
+++ Solution/src/No235.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class No235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> pPath = getPath(root, p);
+        List<TreeNode> qPath = getPath(root, q);
+        TreeNode ret = root;
+        for (int i = 0; i < pPath.size() && i < qPath.size(); i++) {
+            if (pPath.get(i) == qPath.get(i)) {
+                ret = pPath.get(i);
+            }
+        }
+        return ret;
+    }
+
+    private List<TreeNode> getPath(TreeNode root, TreeNode node) {
+        List<TreeNode> path = new ArrayList<>();
+        TreeNode ptr = root;
+        while (ptr != node) {
+            path.add(ptr);
+            if (ptr.val > node.val) {
+                ptr = ptr.left;
+            } else {
+                ptr = ptr.right;
+            }
+        }
+        path.add(ptr);
+        return path;
+    }
+}
