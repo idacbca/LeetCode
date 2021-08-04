c8b195f22993cc77b67f3a3a495dfd950129c4f6
acbca <idacbca@126.com>
2021-08-04 17:22:51 +0800

    add No136

---
+++ Solution/src/No136.java
@@ -0,0 +1,9 @@
+public class No136 {
+    public int singleNumber(int[] nums) {
+        int ret = 0;
+        for (int num : nums) {
+            ret ^= num;
+        }
+        return ret;
+    }
+}
