87c38e680a080382f13a6d72638636684fce7828
acbca <idacbca@126.com>
2021-11-13 02:43:51 -0500

    add No160

---
+++ Solution/src/No160.java
@@ -0,0 +1,22 @@
+public class No160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p = headA;
+        ListNode q = headB;
+
+        while (p != q) {
+            if (p == null) {
+                p = headB;
+            } else {
+                p = p.next;
+            }
+
+            if (q == null) {
+                q = headA;
+            } else {
+                q = q.next;
+            }
+        }
+
+        return p;
+    }
+}
